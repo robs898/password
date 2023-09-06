53d2df6595d8001abfa05af9c261c678aea43727
Robbie <rmackenzie8@gmail.com>
2023-09-06 14:24:12 +0100

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,12 @@
+def generate_password(length=14):
+    """https://docs.python.org/3/library/secrets.html"""
+    alphabet = f"@#&*-_{string.ascii_letters}{string.digits}"
+    while True:
+        password = "".join(secrets.choice(alphabet) for _ in range(length))
+        if (
+            any(c.islower() for c in password)
+            and any(c.isupper() for c in password)
+            and any(c.isdigit() for c in password)
+            and any(c in "@#&*-_" for c in password)
+        ):
+            return password
